caf9e7f8476fe35bf86a63dacb9217fe261f893e
ktan185 <ktan185@aucklanduni.ac.nz>
2023-09-01 15:57:05 +1200

    feat: implemented phone across all rooms.

--- src/main/java/nz/ac/auckland/se206/controllers/LeftRoomController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/LeftRoomController.java
@@ -1,25 +1,28 @@
 package nz.ac.auckland.se206.controllers;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
-import nz.ac.auckland.se206.App;
-import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.TimerClass;
 import nz.ac.auckland.se206.TimerObserver;
+import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
 
 /** Controller class for the room view. */
-public class LeftRoomController implements TimerObserver {
-  @FXML private Text objective;
+public class LeftRoomController extends Phone implements TimerObserver {
+  
+  @FXML private TextArea objective;
   @FXML private Text timer;
+  @FXML private ImageView room;
+  @FXML private TextArea commanderBox;
 
-  /** Initializes the room view, it is called when the room loads. */
-  public void initialize() {
-    objective.setText("This is the RIGHT ROOM");
-    System.out.println("Right init");
+  /** Initializes the room view, it is called when the room loads. 
+   * @throws ApiProxyException */
+  public void initialize() throws ApiProxyException {
+    // Initialise phone.
+    super.initialize();
+    objective.setText("This is the LEFT ROOM");
     TimerClass.add(this);
   }
 
@@ -49,66 +52,4 @@ public class LeftRoomController implements TimerObserver {
     System.out.println("key " + event.getCode() + " released");
   }
 
-  /**
-   * Displays a dialog box with the given title, header text, and message.
-   *
-   * @param title the title of the dialog box
-   * @param headerText the header text of the dialog box
-   * @param message the message content of the dialog box
-   */
-  private void showDialog(String title, String headerText, String message) {
-    Alert alert = new Alert(Alert.AlertType.INFORMATION);
-    alert.setTitle(title);
-    alert.setHeaderText(headerText);
-    alert.setContentText(message);
-    alert.showAndWait();
-  }
-
-  /**
-   * Handles the click event on the door.
-   *
-   * @param event the mouse event
-   * @throws IOException if there is an error loading the chat view
-   */
-  @FXML
-  public void clickDoor(MouseEvent event) throws IOException {
-    System.out.println("door clicked");
-
-    if (!GameState.isRiddleResolved) {
-      showDialog("Info", "Riddle", "You need to resolve the riddle!");
-      App.setRoot("chat");
-      return;
-    }
-
-    if (!GameState.isKeyFound) {
-      showDialog(
-          "Info", "Find the key!", "You resolved the riddle, now you know where the key is.");
-    } else {
-      showDialog("Info", "You Won!", "Good Job!");
-    }
-  }
-
-  /**
-   * Handles the click event on the vase.
-   *
-   * @param event the mouse event
-   */
-  @FXML
-  public void clickVase(MouseEvent event) {
-    System.out.println("vase clicked");
-    if (GameState.isRiddleResolved && !GameState.isKeyFound) {
-      showDialog("Info", "Key Found", "You found a key under the vase!");
-      GameState.isKeyFound = true;
-    }
-  }
-
-  /**
-   * Handles the click event on the window.
-   *
-   * @param event the mouse event
-   */
-  @FXML
-  public void clickWindow(MouseEvent event) {
-    System.out.println("window clicked");
-  }
 }
---
+++ src/main/java/nz/ac/auckland/se206/controllers/Phone.java
@@ -0,0 +1,31 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
+
+public abstract class Phone {
+
+  @FXML protected TextArea input;
+  @FXML protected TextArea output;
+  @FXML protected Button send;
+
+  @FXML
+  protected void initialize() throws ApiProxyException {
+    CommanderController.getInstance().addTextArea(output);
+    if (output != null) {
+      output.setWrapText(true);
+    }
+  }
+
+  @FXML 
+  protected void onSendMessage(MouseEvent event) throws ApiProxyException, IOException {
+    // Send the message to the commander controller.
+    CommanderController.getInstance().onSendMessage(event,input);
+  }
+  
+}
