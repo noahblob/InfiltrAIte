135c1f986b2699d7fa21a3bd3bc0d3f35f218f73
noahblob <noahhagardent@gmail.com>
2023-09-18 21:00:13 +1200

    feat: Added run gpt task to computercontroller and added cover for keypad in mainroom

--- src/main/java/nz/ac/auckland/se206/controllers/main/ComputerController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/main/ComputerController.java
@@ -1,26 +1,84 @@
 package nz.ac.auckland.se206.controllers.main;
 
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import nz.ac.auckland.se206.Commander;
 import nz.ac.auckland.se206.TimerClass;
 import nz.ac.auckland.se206.TimerObserver;
+import nz.ac.auckland.se206.gpt.ChatMessage;
+import nz.ac.auckland.se206.gpt.GptPromptEngineering;
+import nz.ac.auckland.se206.gpt.openai.ApiProxyException;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionRequest;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionResult;
+import nz.ac.auckland.se206.gpt.openai.ChatCompletionResult.Choice;
 
 public class ComputerController extends Commander implements TimerObserver {
 
   @FXML private Label passwordHint;
   @FXML private TextArea computerPassword;
+  private ChatCompletionRequest chatCompletionRequest;
 
   public void initialize() throws Exception {
     super.initialize();
     objective.setText("Figure out the combination!");
     TimerClass.add(this);
+
+    chatCompletionRequest =
+        new ChatCompletionRequest().setN(1).setTemperature(0.2).setTopP(0.5).setMaxTokens(100);
+    runGpt(new ChatMessage("user", GptPromptEngineering.getPasswordRiddle()));
   }
 
   @Override
   public void timerStart() {
     TimerClass timerText = TimerClass.getInstance();
     timer.setText(timerText.getTimerLeft());
+  }
+
+  /**
+   * Runs the GPT model with a given chat message.
+   *
+   * @param msg the chat message to process
+   * @return the response chat message
+   * @throws ApiProxyException if there is an error communicating with the API proxy
+   */
+  private void runGpt(ChatMessage msg) throws ApiProxyException {
+
+    // Create a task for chat gpt to generate password hint asynchronously
+    Task<ChatMessage> taskGpt =
+        new Task<>() {
+          @Override
+          protected ChatMessage call() {
+            chatCompletionRequest.addMessage(msg);
+            try {
+              ChatCompletionResult chatCompletionResult = chatCompletionRequest.execute();
+              Choice result = chatCompletionResult.getChoices().iterator().next();
+              return result.getChatMessage();
+            } catch (ApiProxyException e) {
+              e.printStackTrace();
+              return null;
+            }
+          }
+        };
+    // if task succeeds, set the password hint label to generated password hint
+    taskGpt.setOnSucceeded(
+        e -> {
+          ChatMessage result = taskGpt.getValue();
+          chatCompletionRequest.addMessage(result);
+          passwordHint.setText("paswsword hint: " + result.getContent());
+        });
+
+    // Optional: catch any exceptions thrown during the task execution.
+    taskGpt.setOnFailed(
+        workerStateEvent -> {
+          Throwable ex = taskGpt.getException();
+          ex.printStackTrace();
+        });
+
+    // Run the task in a background thread
+    Thread thread = new Thread(taskGpt);
+    thread.setDaemon(true);
+    thread.start();
   }
 }
