1543c849ab3829cff490b4421dc9cccdbad842bf
ktan185 <ktan185@aucklanduni.ac.nz>
2023-10-04 12:17:34 +1300

    feat: implemented sound effects for time select screen.

--- src/main/java/nz/ac/auckland/se206/controllers/TimeController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/TimeController.java
@@ -8,7 +8,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 import nz.ac.auckland.se206.Dialogue;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.TimerClass;
@@ -25,10 +28,18 @@ public class TimeController {
   @FXML private Button set;
   @FXML private Button back;
   private int gameTime;
+  private Media startSound;
+  private Media interfaceSound;
+  private MediaPlayer start;
+  private MediaPlayer buttons;
+  private static final int TIME_INCREMENT = 2;
+  private static final int MAX_GAME_TIME = 6;
+  private static final int MIN_GAME_TIME = 2;
 
   public void initialize() {
 
     gameTime = 4;
+    setUpSound();
     bindDifficulty();
     setFont();
     updateTime();
@@ -39,6 +50,21 @@ public class TimeController {
     Font.loadFont(getClass().getResourceAsStream("/fonts/DS-DIGI.TTF"), 20);
     diff.setStyle("-fx-font-family: 'DS-Digital'; -fx-font-size: 25px; -fx-text-fill: black;");
     time.setStyle("-fx-font-family: 'DS-Digital'; -fx-font-size: 45px; -fx-text-fill: black;");
+  }
+
+  private void setUpSound() {
+    String startSoundURL = getClass().getResource("/sounds/clickMenu.mp3").toString();
+    String ui = getClass().getResource("/sounds/clickMenu1.mp3").toString();
+    startSound = new Media(startSoundURL);
+    interfaceSound = new Media(ui);
+    start = new MediaPlayer(startSound);
+    buttons = new MediaPlayer(interfaceSound);
+  }
+
+  private void initialiseButtons() {
+    increase.setOnAction(event -> adjustGameTime(TIME_INCREMENT));
+    decrease.setOnAction(event -> adjustGameTime(-TIME_INCREMENT));
+    back.setOnAction(event -> navigateBack());
   }
 
   private void bindDifficulty() {
@@ -57,40 +83,47 @@ public class TimeController {
                 GameState.difficulty));
   }
 
-  private void initialiseButtons() {
-    increase.setOnAction(
-        event -> {
-          decrease.setDisable(false);
-          gameTime += 2; // Increment by 2 minutes
-          if (gameTime > 4) {
-            gameTime = 6; // Ensure time doesn't go above 6 minutes.
-            increase.setDisable(true);
-          }
-          updateTime();
-        });
+  private void adjustGameTime(int delta) {
+    clickSound(buttons);
+    gameTime += delta;
 
-    decrease.setOnAction(
-        event -> {
-          increase.setDisable(false);
-          gameTime -= 2; // Decrement by 2 minutes
-          if (gameTime < 4) {
-            gameTime = 2; // Ensure time doesn't go below 2 minutes
-            decrease.setDisable(true);
-          }
-          updateTime();
-        });
+    boolean shouldDisableIncrease = false;
+    boolean shouldDisableDecrease = false;
 
-    back.setOnAction(
-        event -> {
-          Scene currentScene = back.getScene();
-          currentScene.setRoot(SceneManager.getuserInterface(AppUi.TITLE));
-        });
+    if (gameTime >= MAX_GAME_TIME) {
+      gameTime = MAX_GAME_TIME;
+      shouldDisableIncrease = true;
+    } else if (gameTime <= MIN_GAME_TIME) {
+      gameTime = MIN_GAME_TIME;
+      shouldDisableDecrease = true;
+    }
+
+    setButtonState(increase, shouldDisableIncrease);
+    setButtonState(decrease, shouldDisableDecrease);
+
+    if (!shouldDisableIncrease && !shouldDisableDecrease) {
+      setButtonState(increase, false);
+      setButtonState(decrease, false);
+    }
+
+    updateTime();
+  }
+
+  private void setButtonState(Button button, boolean isDisabled) {
+    button.setDisable(isDisabled);
+  }
+
+  private void navigateBack() {
+    clickSound(buttons);
+    Scene currentScene = back.getScene();
+    currentScene.setRoot(SceneManager.getuserInterface(AppUi.TITLE));
   }
 
   @FXML
   private void onClick(MouseEvent event) throws Exception {
     Button rectangle = (Button) event.getSource();
     Scene currentScene = rectangle.getScene();
+    clickSound(start);
 
     // Check if Easy, medium or hard and update prompt accordingly.
     if (GameState.difficulty.get() == 1) {
@@ -134,5 +167,11 @@ public class TimeController {
     updateTime();
     increase.setDisable(false);
     decrease.setDisable(false);
+  }
+
+  private void clickSound(MediaPlayer player) {
+    player.stop();
+    player.seek(Duration.ZERO);
+    player.play();
   }
 }
